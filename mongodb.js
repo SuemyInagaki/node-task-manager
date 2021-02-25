c57a20264a7533d327995f95fb642a42a02d0a39
Suemy Inagaki <78806385+suemy-inagaki@users.noreply.github.com>
2021-02-25 13:28:47 -0300

    nosignificant modifications

--- mongodb.js
+++ mongodb.js
@@ -16,6 +16,57 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client)=>{
        db.collection('users').insertOne({
            name: 'Suemy',
            age: 22
+       }, (error, result)=> {
+            if(error){
+                return console.log('Unable to insert user')
+            }
+            console.log(result.ops)
        })
+       db.collection('users').insertMany([{
+            name: 'Jen',
+            age: 28
+       },
+       {
+            name: 'Gunther',
+            age: 27
+       }], (error, result)=>{
+            if(error){
+                return console.log('Unable to insert users')
+            }
+            console.log(result.ops)
+       })
+    }
+})
+
+
+
+/*
+Goal: Insert 3 tasks into a new tasks collection
+
+1. User insertMany to insert the documents
+    - description (string), completed (boolean)
+2. Setup the callback to handle error or print ops
+3. Run the script
+4. Refresh the database in Robo 3t and view data in tasks collection
+*/
+
+MongoClient.connect(connectionURL, {useNewParser: true}, (error, client)=>{
+    if(error){
+        console.log('unable to connect')
+    }
+    else{
+        const db = client.db(databaseName)
+        db.collection('tasks').insertMany([
+            { description: 'Do the dishes', completed: false},
+            { description: 'Study Analysis of Algorithm', completed: false},
+            { description: 'Go to the gym', completed: true }], 
+            (error, result)=>{
+                if(error){
+                    console.log('Unable to insert tasks')
+                }
+                else{
+                    console.log(result.ops)
+                }
+            })
     }
 })
